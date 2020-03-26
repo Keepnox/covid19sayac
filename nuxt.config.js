643e9bd5e339136ec02bec26afb491f38bbc9ce1
kaan <kimbukaan@gmail.com>
2020-03-26 03:34:43 +0300

    remove btn

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,8 @@ module.exports = {
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     "@nuxtjs/axios",
-    "@nuxtjs/pwa"
+    "@nuxtjs/pwa",
+
   ],
   // /*
   //  ** Axios module configuration
