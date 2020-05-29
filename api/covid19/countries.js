4e9317f41a31f7e944744a8140de4da1884517dc
Kaan Şahin <kimbukaan@gmail.com>
2020-05-29 16:07:11 +0300

    change link

--- api/covid19/countries.js
+++ api/covid19/countries.js
@@ -11,22 +11,22 @@ export async function fetchCountries() {
   );
 
   const $ = cheerio.load(covidHtmlContent);
-  const items = $("table#main_table_countries_today > tbody > tr")
+  var items = $("table#main_table_countries_today > tbody > tr")
     .map((index, element) => {
       const $el = $(element);
-      const country = $el.find("td:nth-child(1)").text().trim();
-      const confirmed = parseNumber($el.find("td:nth-child(2)").text());
+      const country = $el.find("td:nth-child(2)").text().trim();
+      const confirmed = parseNumber($el.find("td:nth-child(3)").text());
       const confirmedSinceYesterday = parseNumber(
-        $el.find("td:nth-child(3)").text()
+        $el.find("td:nth-child(4)").text()
       );
-      const death = parseNumber($el.find("td:nth-child(4)").text());
+      const death = parseNumber($el.find("td:nth-child(5)").text());
       const deathSinceYesterday = parseNumber(
-        $el.find("td:nth-child(5)").text()
+        $el.find("td:nth-child(6)").text()
       );
-      const recovered = parseNumber($el.find("td:nth-child(6)").text());
-      const activeCase = parseNumber($el.find("td:nth-child(7)").text());
-      const criticalCase = parseNumber($el.find("td:nth-child(8)").text());
-      const casesPer1M = parseNumber($el.find("td:nth-child(9)").text());
+      const recovered = parseNumber($el.find("td:nth-child(7)").text());
+      const activeCase = parseNumber($el.find("td:nth-child(8)").text());
+      const criticalCase = parseNumber($el.find("td:nth-child(9)").text());
+      const casesPer1M = parseNumber($el.find("td:nth-child(10)").text());
 
       const deathRate = (100 * death) / confirmed;
       const activeRate = (100 * activeCase) / confirmed;
